20735e4dc1f9e94343fb5097eec1582edac70b6c
eakudompong.chanoknan <ceguantz96@outlook.com>
2025-06-16 11:43:10 +0900

    feat: add index controller logic

--- my-app/app/Http/Controllers/PostController.php
+++ my-app/app/Http/Controllers/PostController.php
@@ -2,13 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 use Illuminate\View\View;
 
 class PostController extends Controller
 {
     public function index() :View
     {
-        return view('index',['title' => 'index']);
+        $errors = Session::get('errors');
+        Session::forget('errors');
+
+        $postQuery = Post::with(['user', 'thumbnail', 'tags'])
+            ->whereNull('deleted_at')
+            ->orderBy('created_at', 'desc');
+
+        $posts = $postQuery->get();
+
+        return view('index', [
+            'title' => 'Home Page',
+            'data' => $posts,
+            'errors' => $errors
+        ]);
     }
 }
--- my-app/resources/views/index.blade.php
+++ my-app/resources/views/index.blade.php
@@ -1,4 +1,25 @@
 <x-layout :title="$title">
-    <h1>Welcome.blade.php</h1>
-    {{$title}}
+    <h1>{{ $title }}</h1>
+
+    @if ($errors)
+        <div class="text-red-500">{{ $errors }}</div>
+    @endif
+
+    @forelse ($data as $post)
+        <div class="mb-4">
+            <h2>{{ $post->title }}</h2>
+            <p>By {{ $post->user->user_name }}</p>
+            @if ($post->thumbnail)
+                <img src="{{ asset($post->thumbnail->image_path) }}" alt="Thumbnail" />
+            @endif
+
+            <ul>
+                @foreach ($post->tags as $tag)
+                    <li>{{ $tag->name }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @empty
+        <p>No posts found.</p>
+    @endforelse
 </x-layout>
